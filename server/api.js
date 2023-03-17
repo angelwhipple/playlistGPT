960293bc7823e31782628dcf44823a9d582b7c4c
Angel Whipple <awhipp@mit.edu>
2023-03-17 09:57:03 -0400

    fully implemented artist/song associationg

--- server/api.js
+++ server/api.js
@@ -28,7 +28,7 @@ const { response } = require("express");
 const { default: axios } = require("axios");
 const { redirect } = require("react-router-dom");
 const accessToken =
-  "BQBwyCTYcbzs0GJBHXR73j9eh5hKVWb6CV83NklkuKrB29YGfrM3pYLpGVtkb6C3zef4qLJAKilXmHkkunlzf3hh4jhtv4_Ln7q_SnKDdPkOpugND1IFij1f0tDvSTMIoLMccVdkAfHiXT-vo26nV0UpZhcTJ6H-2RUgd95Smc3ge-KUrSX_GHCU6zdUJazeYhx0mSw";
+  "BQDo5JC8zXdg3_Vco4g8QIJR92QWGj9Bs5znxoyqKN-1cxzJaiO-EZqy4yON467ikvgsnW26uJJmfxOVzxSeq6r1o9N4jqoMWXMIX4OCRR7ybuRfLNmq7FWZNxrsv8yr5q7SQ_4vF9AnJi4T3sJNJwK4wK5u2xm-07DGZBkwfzwkVUilbktXYXeI2_b7vU2hcl2myBY";
 const config = {
   headers: {
     Authorization: "Bearer " + accessToken,
@@ -68,44 +68,48 @@ router.post("/updatemood", (req, res) => {
   User.findById(req.body.id).then((user) => {
     let query = "https://api.spotify.com/v1/search?q=";
     let [song, artist] = ["", ""];
-    if ("artist" in req.body) {
-      query += req.body.artist + "&type=artist";
-      axios.get(query, config).then((response) => {
-        const data = response.data;
-        // get the artist ID of first result
-        artist = data.artists.items[0];
-        // console.log(artist.id);
-      });
-    }
-    if ("song" in req.body) {
-      query += req.body.song + "&type=track";
-      axios.get(query, config).then((response) => {
-        const data = response.data;
-        // get the track ID of first result
-        song = data.tracks.items[0];
-        // console.log(song.id);
-      });
-    }
-    // check if custom mood exists for this user, if not create one
-    if (user.customMoods.includes(req.body.mood)) {
-      Mood.findOneAndUpdate(
-        { creator: req.body.id, mood: req.body.mood },
-        song ? { $push: { songs: song } } : { $push: { artists: artist } }
-      ).then(res.send({}));
-    } else {
-      const customMood = new Mood({
-        mood: req.body.mood,
-        artists: artist !== "" ? [artist.id] : [],
-        songs: song !== "" ? [song.id] : [],
-        creator: req.body.id,
-      });
-      // save custom mood NAME to user's list of custom moods
-      customMood.save().then((mood) => {
-        User.findByIdAndUpdate(req.body.id, { $push: { customMoods: mood.mood } }).then(
-          res.send({})
-        );
-      });
-    }
+
+    const asyncProcess = async () => {
+      if ("artist" in req.body) {
+        query += req.body.artist + "&type=artist";
+        await axios.get(query, config).then((response) => {
+          const data = response.data;
+          // get the artist ID of first result
+          artist = data.artists.items[0].id;
+        });
+      }
+      if ("song" in req.body) {
+        query += req.body.song + "&type=track";
+        await axios.get(query, config).then((response) => {
+          const data = response.data;
+          // get the track ID of first result
+          song = data.tracks.items[0].id;
+        });
+      }
+    };
+
+    asyncProcess().then(() => {
+      // check if custom mood exists for this user, if not create one
+      if (user.customMoods.includes(req.body.mood)) {
+        Mood.findOneAndUpdate(
+          { creator: req.body.id, mood: req.body.mood },
+          song ? { $push: { tracks: song } } : { $push: { artists: artist } }
+        ).then(res.send({}));
+      } else {
+        const customMood = new Mood({
+          mood: req.body.mood,
+          artists: artist ? [artist] : [],
+          tracks: song ? [song] : [],
+          creator: req.body.id,
+        });
+        // save custom mood NAME to user's list of custom moods
+        customMood.save().then((mood) => {
+          User.findByIdAndUpdate(req.body.id, { $push: { customMoods: mood.mood } }).then(
+            res.send({})
+          );
+        });
+      }
+    });
   });
 });
 
