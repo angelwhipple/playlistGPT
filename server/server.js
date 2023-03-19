b3adbcee36ee323cffc1221f437b18fb5f95b7c9
Angel Whipple <awhipp@mit.edu>
2023-03-19 17:02:28 -0400

    edited server.js deploy bug

--- server/server.js
+++ server/server.js
@@ -107,6 +107,10 @@ const port = process.env.PORT || 3000;
 const server = http.Server(app);
 socketManager.init(server);
 
-server.listen(port, () => {
-  console.log(`Server running on port: ${port}`);
-});
+server.listen(
+  port,
+  "0.0.0.0"
+  // () => {
+  //   console.log(`Server running on port: ${port}`);
+  // }
+);
